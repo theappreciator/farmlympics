3308a70b5ad5e7b3db15e65d5c0220d8a0841175
theappreciator <jessjesstheappreciator@gmail.com>
2025-07-27 22:14:38 -0400

    and there were files...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # farmlympics
+
+Home for Farmlympics!
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Farmlympics 2025</h1>
+    </>
+  )
+}
+
+export default App
