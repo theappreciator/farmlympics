bc1698cb684cd8a83ca79cbd7ca196242f5b2920
theappreciator <jessjesstheappreciator@gmail.com>
2025-07-27 23:33:01 -0400

    Add in some people

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,178 @@
+import React from 'react'
 import './App.css'
+import TeamTable from './TeamTable';
+
+// export type Team = "A" | "B";
+
+export const Teams = {
+  TeamA: "A",
+  TeamB: "B",
+  TeamU: "TBD",
+} as const;
+export type Teams = typeof Teams[keyof typeof Teams];
+
+export type Generation = "Silent Gen" | "Baby Boomers" | "Gen X" | "Millennials" | "Gen Z" | "Gen Alpha" | "Little One";
+
+export type Sleeping = "farm room" | "the office" | "basement" | "camper" | "none" | undefined;
+
+export type Person = {
+  name: string
+  generation: Generation
+  team: Teams
+  sleeping: {
+    friday: Sleeping,
+    saturday: Sleeping,
+    sunday: Sleeping,
+  }
+}
+
+const defaultData: Person[] = [
+  {
+    name: 'J. T.',
+    generation: 'Millennials',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'A. T.',
+    generation: 'Millennials',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'J. T.',
+    generation: 'Gen Alpha',
+    team: Teams.TeamB,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'G. T.',
+    generation: 'Gen Alpha',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'B. T.',
+    generation: 'Little One',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'G. D.',
+    generation: 'Gen X',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'D. D.',
+    generation: 'Gen X',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'farm room',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'R. D.',
+    generation: 'Millennials',
+    team: Teams.TeamB,
+    sleeping: {
+      friday: 'none',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'C. D.',
+    generation: 'Millennials',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'none',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'R. D.',
+    generation: 'Gen Alpha',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: 'none',
+      saturday: 'farm room',
+      sunday: 'farm room',
+    },
+  },
+  {
+    name: 'M. F.',
+    generation: 'Millennials',
+    team: Teams.TeamB,
+    sleeping: {
+      friday: undefined,
+      saturday: undefined,
+      sunday: undefined
+    },
+  },
+  {
+    name: 'J. F.',
+    generation: 'Gen Z',
+    team: Teams.TeamB,
+    sleeping: {
+      friday: undefined,
+      saturday: undefined,
+      sunday: undefined
+    },
+  },
+  {
+    name: 'R. L.',
+    generation: 'Millennials',
+    team: Teams.TeamA,
+    sleeping: {
+      friday: "none",
+      saturday: undefined,
+      sunday: undefined
+    },
+  },
+]
+
+
 
 function App() {
+
+  
+
   return (
     <>
       <h1>Farmlympics 2025</h1>
+
+      <TeamTable team={Teams.TeamA} people={defaultData.filter(p => p.team === Teams.TeamA)}/>
+      <TeamTable team={Teams.TeamB} people={defaultData.filter(p => p.team === Teams.TeamB)}/>
+      <TeamTable team={Teams.TeamU} people={defaultData.filter(p => p.team === Teams.TeamU)}/>
+
     </>
   )
 }
---
+++ src/TeamTable.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import './TeamTable.css'
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import type { Person, Sleeping, Teams } from './App'
+
+type TeamTableProps = {
+  team: Teams;
+  people: Person[];
+};
+
+const columnHelper = createColumnHelper<Person>()
+
+const columns = [
+  columnHelper.accessor('name', {
+    header: () => <span>Name</span>,
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('generation', {
+    header: () => <span>Generation</span>,
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('team', {
+    header: () => <span>Team</span>,
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('sleeping.friday', {
+    header: () => <span>Friday</span>,
+    cell: info => sleepingDisplay(info.getValue()),
+  }),
+  columnHelper.accessor('sleeping.saturday', {
+    header: () => <span>Saturday</span>,
+    cell: info => sleepingDisplay(info.getValue()),
+  }),
+  columnHelper.accessor('sleeping.sunday', {
+    header: () => <span>Sunday</span>,
+    cell: info => sleepingDisplay(info.getValue()),
+  }),
+]
+
+const sleepingDisplay = (sleeping: Sleeping) => {
+  if (sleeping === undefined) {
+    return "?"
+  }
+  
+  return sleeping;
+}
+
+const TeamTable: React.FC<TeamTableProps> = ({ team, people }) => {
+
+  const [data, _setData] = React.useState(() => [...people])
+  const rerender = React.useReducer(() => ({}), {})[1]
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <>
+    {people.length > 0 && (
+      <>
+      <div>TEAM {team}</div>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          
+        </table>
+      </>
+    )}
+    </>
+  )
+}
+
+export default TeamTable
